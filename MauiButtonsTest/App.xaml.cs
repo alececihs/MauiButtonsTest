11d53b0deb372253b9818a0d371fbb7c48ce1c9d
alececIHS <alessio.cecchi@infohospital.it>
2024-01-29 10:44:18 +0100

    Initial commit

---
+++ MauiButtonsTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiButtonsTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiButtonsTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiButtonsTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
